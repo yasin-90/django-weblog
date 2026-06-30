80088d438a5016c677c089002f62bbb881f5fb88
Yasin <m.yasinbachari@gmail.com>
2026-07-01 00:35:04 +0330

    edit models

--- weblog/blog/models.py
+++ weblog/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     user =models.CharField(max_length=30)
-    password =models.IntegerField(max_length=30)
+    password =models.CharField(max_length=30)
 
     def __str__(self):
         return f"{self.user}"
